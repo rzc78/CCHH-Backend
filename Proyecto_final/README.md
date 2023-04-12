128eed2e08b7b248938e9a6094a2e99e94f726ee
Rodrigo <rodrigozalazarcavalleris@gmail.com>
2023-04-11 22:39:10 -0300

    Desafio WEBSOCKET: implemetación en combinación con handlebars. Actualización de listado en tiempo real desde la ruta especificada. Funcionando

--- Proyecto_final/README.md
+++ Proyecto_final/README.md
@@ -17,8 +17,30 @@ Este comando iniciará nodemon y ejecutará la aplicación. Si si re hacen cambi
 
 El proyecto utiliza las siguientes dependencias:
 
-- Express (versión 4.18.2): un framework de Node.js para crear aplicaciones web.
+- Express (versión 4.18.2): un framework de Node.js para crear aplicaciones web:
+
+Instalación:
+
+    npm install express
+
 - Express Router (versión 0.0.1): un middleware de Express para definir rutas.
+
+Instalación:
+
+    npm install express-router
+
+- Express Handlebars (versión 7.0.4): Es un sistema de plantillas Javascript basado en Mustache 
+Templates.
+
+Instalación:
+
+    npm install expres-handlebars
+
+- Socket.io (versión 4.6.1): para la incorporación de web socket
+
+Instalación:
+
+    npm install socket.io
 
 ## Dependencias de desarrollo
 
@@ -30,4 +52,4 @@ El proyecto tiene los siguientes scripts definidos:
 
 - test: un script que imprime un mensaje de error si no se han definido pruebas.
 - start: un script que utiliza nodemon para iniciar la aplicación.
-Licencia
+
--- Proyecto_final/src/app.js
+++ Proyecto_final/src/app.js
@@ -1,23 +1,61 @@
-//Importación de express y de ProductManager
+//Importación de express, routes, engine y path;
 import express from "express";
 import ProductRouter from './routes/ProductRouter.js';
 import CartsRouter from './routes/CartRouter.js';
+import ViewsRouter from "./routes/views.router.js";
+import ProductManager from "./productManager.js";
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
 
+//Import de path para manejo de las rutas relativas en node.
+import { resolve } from 'path';
 
 //Inicialización de variables: app para guardar el método de express y port para el puerto 8080
 const app = express();
 const port = 8080;
 
-//middlewares
-app.use(express.json())
+//Llamado a la función getProducts de ProductManager. Se instancia
+const productManager = new ProductManager();
+const list = await productManager.getProducts();
+
+//MIDDELWARES
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Handlebars Config de routes
+const viewsPath = resolve('src/views'); //Dir donde se alojan vistas
+app.engine('handlebars', engine({layoutsDir: `${viewsPath}/layouts`, defaultLayout: `${viewsPath}/layouts/main.handlebars`}));
+
+app.set('view engine', 'handlebars');
+app.set('views', viewsPath )
+
+//Router para Handlebars
+app.use('/', ViewsRouter);
 
 app.use('/api/products', ProductRouter);
 
-app.use('/api/cart', CartsRouter);
+app.use('/api/carts', CartsRouter);
 
 //Listener de puerto en ejecución.
-app.listen(port, () => {
+const httpServer = app.listen(port, () => {
   console.log(`Listening port ${port}. On line!`);
 });
+
+//Instanciado socket
+const socketServer = new Server(httpServer);
+
+// Server en escucha desde el server
+socketServer.on('connection', socket=> {
+  console.log("Cliente Conectado");
+
+  socket.on('message', (data)=>{
+    console.log(data)});
+
+  //Socket Emisor para renderizado en realTimeProducts
+  socket.emit('evento_para_socket_individual', 'Actualizado');
+  socketServer.emit('new-list', list);
+
+});
+
+
 
